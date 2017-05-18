c0a878976cbc64b43be459af853b0a7b89096313
avergnaud <a.vergnaud@gmail.com>
2017-05-18 22:19:41 +0200

    init

--- src/main/java/sparkjava/Route.java
+++ src/main/java/sparkjava/Route.java
@@ -3,16 +3,39 @@ package sparkjava;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import spark.Request;
 import spark.Response;
 
 public class Route {
+	
+	public static void main(String[] args) {
+		String in = "4e 01 4b 46 7f ff 0c 10 05 : crc\u003d05 YES\n4e 01 4b 46 7f ff 0c 10 05 t\u003d20875\n";
+		System.out.println(in);
+	}
+	
+	private static int extractFromFile(String fileContent) {
+		Pattern p = Pattern.compile("a.*") ;  
+		Matcher m = p.matcher(fileContent) ; 
+		String s = "";
+		if(m.find()) {
+			s = m.group();
+		}
+		return Integer.parseInt(s) / 100;
+	}
 
 	public static String handleGetTemperature(Request request, Response response) {
 		String content = "...";
 		try {
-			content = new String(Files.readAllBytes(Paths.get("/home/ubuntu/free")));
+			List<String> liste = Files.readAllLines(Paths.get("/sys/bus/w1/devices/28-051692238bff/w1_slave"));
+			content = liste.get(1);
+			/*
+			 * "c2 01 4b 46 7f ff 0c 10 a8 : crc\u003da8 YES\nc2 01 4b 46 7f ff 0c 10 a8 t\u003d28125\n"
+			 * */
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/main/java/sparkjava/Server.java
+++ src/main/java/sparkjava/Server.java
@@ -4,12 +4,18 @@ import static spark.Spark.get;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import static spark.Spark.staticFileLocation;
+import static spark.Spark.port;
 
 public class Server {
 
 	public static void main(String[] args) {
 
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		staticFileLocation("/public");
+		
+		port(5678);
 
 		// http://localhost:4567/getByName/adrien
 		get("/getTemperature", Route::handleGetTemperature, gson::toJson);
